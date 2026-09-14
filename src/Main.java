1b7b637e2a3e7d5ec70ab5939fd05abbf21bdadc
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-14 11:10:25 +0200

    h

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,41 @@
 import java.util.regex.*;
+
 public class Main {
     public static void main(String[] args) {
-        String text1 = " Hello 2025! 123 ";
-        Pattern p1 = Pattern.compile(" [0-9]+"); // slice
-        Matcher m1 = p1.matcher(text1);
-        if(m1.find()) {
-            System.out.println(" Nalezeno : " + m1.group());
+
+        String text = "Hello 2025! 123 1";
+        Pattern p = Pattern.compile("[0-9]+"); // slice
+        Matcher m = p.matcher(text);
+        if (m.find()) {
+            System.out.println(" Nalezeno : " + m.group()); // -> 2025
         }
-
-        while(m1.find()) {
-            System.out.println(" Nalezeno : " + m1.group()); // -> 2025 , 123
-        }
-
-
-        String text2 = " Hello 2025! 123 ";
-        Pattern p2 = Pattern.compile("Hello"); // slice
-        Matcher m2 = p2.matcher(text2);
-        if(m2.find()) {
-            System.out.println(" Nalezeno : " + m2.group());
+        while (m.find()) {
+            System.out.println(" Nalezeno : " + m.group()); // -> 2025 , 123
         }
 
 
-        String text3 = " Hello 2025! 123 ";
-        Pattern p3 = Pattern.compile("[A-Z]"); // slice
-        Matcher m3 = p3.matcher(text3);
-        if(m3.find()) {
-            System.out.println(" Nalezeno : " + m3.group());
+        Pattern p2 = Pattern.compile("Hello");
+        Matcher m2 = p2.matcher(text);
+        if (m2.find()) {
+            System.out.println(" Nalezeno2 : " + m2.group());
         }
 
-
-        String text9 = " Hello 2025! 123 ";
-        Pattern p9 = Pattern.compile("[a-z|A-Z]"); // slice
-        Matcher m9 = p9.matcher(text9);
-        if(m9.find()) {
-            System.out.println(" Nalezeno : " + m9.group());
+        Pattern p3 = Pattern.compile("[A-Z]+");
+        Matcher m3 = p3.matcher(text);
+        if (m3.find()) {
+            System.out.println(" Nalezeno3 : " + m3.group());
         }
 
+        Pattern p4 = Pattern.compile("[a-z|A-Z]+");
+        Matcher m4 = p4.matcher(text);
+        if (m4.find()) {
+            System.out.println(" Nalezeno4 : " + m4.group());
+        }
 
-        String text4 = " Hello 2025! 123 ";
-        Pattern p4 = Pattern.compile("/d"); // slice
-        Matcher m4 = p4.matcher(text4);
-        if(m4.find()) {
-            System.out.println(" Nalezeno : " + m4.group());
+        Pattern p5 = Pattern.compile("/d");
+        Matcher m5 = p5.matcher(text);
+        if (m5.find()) {
+            System.out.println(" Nalezeno5 : " + m5.group());
         }
 
     }
